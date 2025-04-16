b2325efe6d7092812aac1b7b8bf1714f5754deec
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-04-17 01:08:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (29.35%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int ans=INT_MAX;
+        int i=0;
+        int j=0;
+        int n=blocks.length();
+        unordered_map<char,int>mp;
+        while(j<n){
+            mp[blocks[j]]++;
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                ans=min(ans,mp['W']);
+                mp[blocks[i]]--;
+                if(mp[blocks[i]]==0){
+                    mp.erase(blocks[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
