8478cb9d99731106f0eab5a90d4093cd16aa531c
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-06-01 21:48:45 +0530

    Time: 3 ms (65.98%), Space: 10.3 MB (54.85%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canPlace(int row,int col,vector<string>&grid,int n){
+        for(int i=row-1;i>=0;i--){
+            if(grid[i][col]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(grid[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(grid[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(int n,vector<vector<string>>&out,vector<string>&nQ, int row){
+        if(row==n){
+            out.push_back(nQ);
+            return;
+        }
+        for(int col=0;col<n;col++){
+            if(canPlace(row,col,nQ,n)){
+                nQ[row][col]='Q';
+                solve(n,out,nQ,row+1);
+                nQ[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>out;
+        vector<string> nQ(n, string(n, '.')); 
+        solve(n,out,nQ,0);
+        return out;
+    }
+};
