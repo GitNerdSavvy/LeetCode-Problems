055dca64c045e91b0600d09d5356341fff64306f
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-06-02 21:04:09 +0530

    Time: 0 ms (100%), Space: 10.4 MB (35.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     void code(vector<int>&nums,vector<int>ans,vector<vector<int>>&res,int index){
-        if(index==nums.size()){
-            res.push_back(ans);
-            return;
+        res.push_back(ans);
+        for(int i=index;i<nums.size();i++){
+            ans.push_back(nums[i]);
+            code(nums,ans,res,i+1);
+            ans.pop_back();
         }
-        code(nums, ans, res,index+1);
-        ans.push_back(nums[index]);
-        code(nums, ans, res,index+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>ans;
