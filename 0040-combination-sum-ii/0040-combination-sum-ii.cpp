c3e828e273c12c2c646d650e797aceffd3224b8d
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-06-03 16:57:31 +0530

    Time: 0 ms (100%), Space: 14.3 MB (31.6%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(vector<int>&cnd,int t,int idx,vector<int>&res){
+        if(t==0){
+            ans.push_back(res);
+            return;
+        }
+        if(idx==cnd.size()) return;
+
+        if(cnd[idx]<=t){
+            res.push_back(cnd[idx]);
+            solve(cnd,t-cnd[idx],idx+1,res);
+            res.pop_back();
+        }
+
+        int j=idx+1;
+        while(j<cnd.size() && cnd[j]==cnd[j-1]) j++;
+        solve(cnd,t,j,res);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& cnd, int target) {
+        sort(cnd.begin(),cnd.end());
+        vector<int>res;
+        ans.clear();
+        solve(cnd,target,0,res);
+        return ans;
+    }
+};
