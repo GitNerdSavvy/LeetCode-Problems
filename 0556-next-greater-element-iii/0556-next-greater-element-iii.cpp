bd09bdab6393a821a6105e0a55afdb22dd0bb13c
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-06-15 10:43:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (27.32%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
-    int nextGreaterElement(int n) {
-        string ans = to_string(n);
-    if (!next_permutation(ans.begin(), ans.end())) return -1;
-    long long res = stoll(ans);
-    return (res > INT_MAX) ? -1 : res; 
+    int nextGreaterElement(int ni) {
+        string s= to_string(ni);
+        int i,j;
+        int n=s.length();
+        for(i=n-2;i>=0;i--){
+            if(s[i] < s[i+1]) break;
+        }
+        if(i == -1) return -1;
+        for(j = n-1; j>=0;j--){
+            if(s[j] > s[i]) break;
+        }
+        swap(s[i], s[j]);
+        reverse(s.begin() + i + 1, s.end());
+        long long res = stoll(s);
+        if(res > INT_MAX) return -1;
+        return res;
     }
 };
