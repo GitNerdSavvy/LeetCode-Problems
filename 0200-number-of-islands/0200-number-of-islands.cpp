418684d2459668bf956123706d22c7db720eb4b1
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-17 01:09:03 +0530

    Time: 26 ms (64.61%), Space: 23.3 MB (34.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int r,int c){
-        if(r<0 || c<0 || r>=grid.size() || c>= grid[0].size() || grid[r][c]=='0') return;
-        grid[r][c]='0';
-        dfs(grid,r+1,c);
-        dfs(grid,r,c+1);
-        dfs(grid,r-1,c);
-        dfs(grid,r,c-1);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
                     cnt++;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        auto curr = q.front();
+                        q.pop();
+                        int r = curr.first;
+                        int c = curr.second;
+                        if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                            q.push({r, c - 1});
+                            grid[r][c - 1] = '0';
+                        }
+                        if (c + 1 < m && grid[r][c + 1] == '1') {
+                            q.push({r, c + 1});
+                            grid[r][c + 1] = '0';
+                        }
+                        if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                            q.push({r - 1, c});
+                            grid[r - 1][c] = '0';
+                        }
+                        if (r + 1 <n && grid[r + 1][c] == '1') {
+                            q.push({r + 1, c});
+                            grid[r + 1][c] = '0';
+                        }
+                    }
                 }
             }
         }
