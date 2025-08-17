c159fe515fffda4eaa00bc30f845d52cb7ba9455
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-17 23:56:36 +0530

    Time: 2 ms (61.59%), Space: 27.5 MB (72.99%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int row, col;
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= row || c >= col || grid[r][c] == 0)
-            return 0;
 
+    void dfs(vector<vector<int>>& grid, int r, int c, int& cnt) {
+        if (r < 0 || c < 0 || r >= row || c >= col || grid[r][c] == 0)
+            return;
         grid[r][c] = 0;
-        return 1 + dfs(grid, r + 1, c) + dfs(grid, r - 1, c) +
-               dfs(grid, r, c + 1) + dfs(grid, r, c - 1);
+        cnt++;
+        dfs(grid, r + 1, c, cnt);
+        dfs(grid, r - 1, c, cnt);
+        dfs(grid, r, c + 1, cnt);
+        dfs(grid, r, c - 1, cnt);
     }
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         row = grid.size();
         col = grid[0].size();
@@ -16,7 +20,9 @@ public:
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (grid[i][j] == 1) {
-                    ans = max(ans, dfs(grid, i, j));
+                    int cnt = 0;
+                    dfs(grid, i, j, cnt);
+                    ans = max(ans, cnt);
                 }
             }
         }
