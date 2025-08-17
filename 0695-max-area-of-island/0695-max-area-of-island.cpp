6ea4c6e0bfe446b141624e1ec1bb069e11030260
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-18 00:09:41 +0530

    Time: 0 ms (100%), Space: 29 MB (36.73%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    int row, col;
-
-    void dfs(vector<vector<int>>& grid, int r, int c, int& cnt) {
-        if (r < 0 || c < 0 || r >= row || c >= col || grid[r][c] == 0)
-            return;
-        grid[r][c] = 0;
-        cnt++;
-        dfs(grid, r + 1, c, cnt);
-        dfs(grid, r - 1, c, cnt);
-        dfs(grid, r, c + 1, cnt);
-        dfs(grid, r, c - 1, cnt);
-    }
-
+    vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        row = grid.size();
-        col = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
         int ans = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    int cnt = 0;
-                    dfs(grid, i, j, cnt);
+                    q.push({i, j});
+                    grid[i][j] = 0;
+                    int cnt = 1;
+
+                    while (!q.empty()) {
+                        auto [row, col] = q.front();
+                        q.pop();
+
+                        for (int d = 0; d < 4; d++) {
+                            int r = row + dir[d][0];
+                            int c = col + dir[d][1];
+                            if (r < 0 || c < 0 || r >= n || c >= m)
+                                continue;
+                            if (grid[r][c] == 0)
+                                continue;
+                            grid[r][c] = 0;
+                            cnt++;
+                            q.push({r, c});
+                        }
+                    }
                     ans = max(ans, cnt);
                 }
             }
         }
+
         return ans;
     }
 };
