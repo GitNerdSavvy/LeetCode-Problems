8dc327b32b9724c302370051670ce98c71eb40a2
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-18 18:16:32 +0530

    Time: 25 ms (71.89%), Space: 66.8 MB (71.33%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int n, m;
+    vector<vector<bool>> vis;
+    bool dfs(vector<vector<char>>& grid, int i, int j, int px, int py,
+             char ch) {
+        vis[i][j] = true;
+        for (auto [dx, dy] : dir) {
+            int nx = i + dx, ny = j + dy;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m)
+                continue;
+            if (grid[nx][ny] != ch)
+                continue;
+            if (nx == px && ny == py)
+                continue;
+            if (vis[nx][ny])
+                return true;
+            if (dfs(grid, nx, ny, i, j, ch))
+                return true;
+        }
+        return false;
+    }
+
     bool containsCycle(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        n = grid.size();
+        m = grid[0].size();
+        vis.assign(n, vector<bool>(m, false));
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (!vis[i][j]) {
-                    char ch = grid[i][j];
-                    queue<tuple<int, int, int, int>> q;
-                    vis[i][j] = true;
-                    q.push({i, j, -1, -1});
-
-                    while (!q.empty()) {
-                        auto [x, y, px, py] = q.front();
-                        q.pop();
-
-                        for (int d = 0; d < 4; d++) {
-                            int r = x + dir[d][0];
-                            int c = y + dir[d][1];
-                            if (r < 0 || c < 0 || r >= n || c >= m)
-                                continue;
-                            if (grid[r][c] != ch)
-                                continue;
-
-                            if (vis[r][c]) {
-                                if (!(r == px && c == py))
-                                    return true;
-                            } else {
-                                vis[r][c] = true;
-                                q.push({r, c, x, y});
-                            }
-                        }
+                    if (dfs(grid, i, j, -1, -1, grid[i][j])) {
+                        return true;
                     }
                 }
             }
