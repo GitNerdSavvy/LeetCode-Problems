22201a66afcf4812e1d992b5868c33a7de00167e
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-25 00:11:49 +0530

    Time: 34 ms (8.29%), Space: 108.5 MB (9.79%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -2,33 +2,52 @@ class Solution {
 public:
     vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     int n, m;
-    int dfs(vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m)
-            return 1;
-        if (grid[i][j] == 0)
-            return 1;
-        if (grid[i][j] == -1)
-            return 0;
-        int cnt = 0;
-        grid[i][j] = -1;
-        for (int d = 0; d < 4; d++) {
-            int row = i + dir[d][0];
-            int col = j + dir[d][1];
-            cnt += dfs(grid, row, col);
-        }
-        return cnt;
-    }
+    // int dfs(vector<vector<int>>& grid, int i, int j) {
+    //     if (i < 0 || j < 0 || i >= n || j >= m)
+    //         return 1;
+    //     if (grid[i][j] == 0)
+    //         return 1;
+    //     if (grid[i][j] == -1)
+    //         return 0;
+    //     int cnt = 0;
+    //     grid[i][j] = -1;
+    //     for (int d = 0; d < 4; d++) {
+    //         int row = i + dir[d][0];
+    //         int col = j + dir[d][1];
+    //         cnt += dfs(grid, row, col);
+    //     }
+    //     return cnt;
+    // }
     int islandPerimeter(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-
+        queue<pair<int, int>> q;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    return dfs(grid, i, j);
+                    q.push({i, j});
+                    grid[i][j]=-1;
+                    break;
                 }
             }
         }
-        return 0;
+        int cnt = 0;
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            for (int d = 0; d < 4; d++) {
+                int r = row + dir[d][0];
+                int c = col + dir[d][1];
+                if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] == 0) {
+                    cnt++;
+                    continue;
+                }
+                if (grid[r][c] == 1) {
+                    q.push({r, c});
+                }
+                grid[r][c] = -1;
+            }
+        }
+        return cnt;
     }
 };
