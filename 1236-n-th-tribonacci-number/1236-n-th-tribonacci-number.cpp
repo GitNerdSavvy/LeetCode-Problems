7caa95367ad3aa998589e70b0abab51ce8e336e1
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-08-30 22:04:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (6.93%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<int> dp;
-    int dfs(int n) {
+    // int dfs(int n) {
+    //     if (n == 0)
+    //         return 0;
+    //     if (n == 1 || n == 2)
+    //         return 1;
+    //     if (dp[n] != -1)
+    //         return dp[n];
+    //     return dp[n] = dfs(n - 1) + dfs(n - 2) + dfs(n - 3);
+    // }
+    int tribonacci(int n) {
         if (n == 0)
             return 0;
         if (n == 1 || n == 2)
             return 1;
-        if (dp[n] != -1)
-            return dp[n];
-        return dp[n] = dfs(n - 1) + dfs(n - 2) + dfs(n - 3);
-    }
-    int tribonacci(int n) {
         dp.assign(n + 1, -1);
-        return dfs(n);
+        dp[0] = 0;
+        dp[1] = 1, dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
     }
 };
