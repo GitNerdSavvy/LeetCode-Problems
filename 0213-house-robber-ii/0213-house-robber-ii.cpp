2eafd7dec55c05aaea36f35dbb0fc97bb0bdfe1f
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-09-10 12:05:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (5.07%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int solve(vector<int>& v, int n, int i, vector<int>& dp) {
+        if (i >= n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int eve = v[i] + solve(v, n, i + 2, dp);
+        int odd = solve(v, n, i + 1, dp);
+        dp[i] = max(eve, odd);
+        return dp[i];
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(nums.size()==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int>nums2=nums;
+        nums2.push_back(nums[0]);
+         vector<int> dp(n, -1);
+        int m1=solve(nums,nums.size()-1,0,dp);
+        dp.assign(n+1,-1);
+        int m2=solve(nums2,nums2.size()-1,1,dp);
+        return max(m1,m2);
+    }
+};
