5acce91fd0ffabf9f8edeb3070c62b9e67fd27e9
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-09-11 23:02:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (62.58%) - LeetHub

--- 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.cpp
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     string winningPlayer(int x, int y) {
-        int cnt=0;
-        while(x>0 && y>3){
-            cnt++;
-            x--;
-            y-=4;
-        }
-        if(cnt%2){
+        if(min(x,y/4)%2){
             return "Alice";
         }else{
             return "Bob";
