5d74f482c2c14ad06be88840101ec260e2a785d3
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-09-14 00:31:08 +0530

    Time: 3 ms (40.46%), Space: 10.3 MB (73.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
-void removeExtraSpaces(string &s) {
-    int n = s.length();
-    int i = 0, j = 0;
-    
-    while (i < n && s[i] == ' ') i++;
-    
-    bool spaceFound = false;
-    while (i < n) {
-        if (s[i] != ' ') {
-            if (spaceFound) s[j++] = ' ';
-            s[j++] = s[i++];
-            spaceFound = false;
-        } else {
-            while (i < n && s[i] == ' ') i++;
-            spaceFound = true;
+    string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        stringstream ss(s);
+        string word;
+        string ans="";
+        while(ss>>word){
+            reverse(word.begin(),word.end());
+           if(ans !="") ans+=" ";
+            ans+=word;
         }
-    }
-    
-    s.resize(j);
-}
-
-string reverseWords(string s) {
-    reverse(s.begin(), s.end());
-
-    int n = s.length();
-    int l = 0;
-
-    for (int i = 0; i <= n; ++i) {
-        if (i == n || s[i] == ' ') {
-            reverse(s.begin() + l, s.begin() + i);
-            l = i + 1;
-        }
-    }
-
-    removeExtraSpaces(s);
-    return s;
+        return ans;
     }
 };
