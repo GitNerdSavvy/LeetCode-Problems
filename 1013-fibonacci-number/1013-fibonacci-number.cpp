6760732d21f25b59325cd7f9793ab0ae71218dbd
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-09-17 11:53:23 +0530

    Time: 3 ms (66.45%), Space: 7.8 MB (36.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,54 @@
 class Solution {
 public:
-   int fib1(int n, vector<int>& dp) {
-    if(n == 0 || n == 1) return n;
-    if(dp[n] != -1) return dp[n];
-    dp[n] = fib1(n - 1, dp) + fib1(n - 2, dp);
-    return dp[n];
-  }
-    int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return fib1(n,dp);
+    struct matrix {
+        int mat[2][2];
+    };
+
+    matrix mul(matrix A, matrix B) {
+        matrix C;
+        for (int row = 0; row < 2; row++) {
+            for (int col = 0; col < 2; col++) {
+                C.mat[row][col] = 0;
+                for (int i = 0; i < 2; i++) {
+                    C.mat[row][col] += A.mat[row][i] * B.mat[i][col];
+                }
+            }
+        }
+        return C;
+    }
+
+    matrix identity() {
+        matrix A;
+        A.mat[0][0] = 1;
+        A.mat[0][1] = 0;
+        A.mat[1][0] = 0;
+        A.mat[1][1] = 1;
+        return A;
+    }
+
+    matrix power(matrix A, int N) {
+        if (N == 0) {
+            return identity();
+        }
+
+        matrix X = power(A, N / 2);
+        if (N % 2 == 0) {
+            return mul(X, X);
+        } else {
+            return mul(mul(X, X), A);
+        }
+    }
+
+    int fib(int N) {
+        if (N == 0)
+            return 0;
+
+        matrix fib;
+        fib.mat[0][0] = 1;
+        fib.mat[0][1] = 1;
+        fib.mat[1][0] = 1;
+        fib.mat[1][1] = 0;
+
+        return power(fib, N - 1).mat[0][0];
     }
 };
