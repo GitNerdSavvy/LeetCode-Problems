46028bd45710b845f09283a27d602f76c2577ec9
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-09-30 14:42:27 +0530

    Time: 0 ms (100%), Space: 8.1 MB (45.46%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,26 +1,54 @@
 class Solution {
 public:
-    vector<int> dp;
-    // int dfs(int n) {
-    //     if (n == 0)
-    //         return 0;
-    //     if (n == 1 || n == 2)
-    //         return 1;
-    //     if (dp[n] != -1)
-    //         return dp[n];
-    //     return dp[n] = dfs(n - 1) + dfs(n - 2) + dfs(n - 3);
-    // }
-    int tribonacci(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1 || n == 2)
-            return 1;
-        dp.assign(n + 1, -1);
-        dp[0] = 0;
-        dp[1] = 1, dp[2] = 1;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    class matrix {
+public:
+    int mat[3][3];
+};
+matrix mul(matrix A, matrix B) {
+    matrix c;
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            int t = 0;
+
+            for (int i = 0; i < 3; i++) {
+                t += A.mat[row][i] * B.mat[i][col];
+            }
+            c.mat[row][col] = t;
         }
-        return dp[n];
+    }
+    return c;
+}
+
+matrix identity() {
+    matrix A;
+    A.mat[0][0] = 1;
+    A.mat[0][1] = 0;
+    A.mat[0][2] = 0;
+    A.mat[1][0] = 0;
+    A.mat[1][1] = 1;
+    A.mat[1][2] = 0;
+    A.mat[2][0] = 0;
+    A.mat[2][1] = 0;
+    A.mat[2][2] = 1;
+    return A;
+}
+
+matrix power(matrix trib, int n) {
+    if (n == 0)
+        return identity();
+    matrix x = power(trib, n / 2);
+    if (n & 1) {
+        return mul(mul(x, x), trib);
+    } else {
+        return mul(x, x);
+    }
+}
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        matrix trib;
+        trib.mat[0][0] = 1, trib.mat[0][1] = 1, trib.mat[0][2] = 1;
+        trib.mat[1][0] = 1, trib.mat[1][1] = 0, trib.mat[1][2] = 0;
+        trib.mat[2][0] = 0, trib.mat[2][1] = 1, trib.mat[2][2] = 0;
+        return power(trib, n-1).mat[0][0];
     }
 };
