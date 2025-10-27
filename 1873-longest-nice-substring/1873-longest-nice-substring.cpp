3215178a4c84de90c56fac0db3748468c99c174b
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-10-27 19:53:50 +0530

    Time: 3 ms (88.57%), Space: 14.3 MB (47.79%) - LeetHub

--- 1873-longest-nice-substring/1873-longest-nice-substring.cpp
+++ 1873-longest-nice-substring/1873-longest-nice-substring.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    bool nice(string t) {
-        set<char> s(t.begin(), t.end());
-        for (char c : s) {
-            if (islower(c)) {
-                if (s.count(toupper(c)) == 0)
-                    return false;
-            } else {
-                if (s.count(tolower(c)) == 0)
-                    return false;
+    string nice(string s) {
+        if (s.size() < 2)
+            return "";
+
+        set<char> st(s.begin(), s.end());
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (st.count(tolower(c)) == 0 || st.count(toupper(c)) == 0) {
+                string left = nice(s.substr(0, i));
+                string right = nice(s.substr(i + 1));
+                return left.size() >= right.size() ? left : right;
             }
         }
-        return true;
+        return s;
     }
-    string longestNiceSubstring(string s) {
-        int n = s.length();
-        string ans = "";
-        for (int i = 0; i < n; i++) {
-            string t = "";
-            for (int j = i; j < n; j++) {
-                t += s[j];
-                if (nice(t)) {
-                    if (ans.length() < t.length()) {
-                        ans = t;
-                    }
-                }
-            }
-        }
-        return ans;
-    }
+    string longestNiceSubstring(string s) { return nice(s); }
 };
