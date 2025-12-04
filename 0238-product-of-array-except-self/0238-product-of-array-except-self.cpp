0247c0e7db2b81b60987256e3463609caaea9adc
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-12-04 18:15:10 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int>res(n);
-        res[0]=nums[0];
-        for(int i=1;i<n;i++){
-            res[i]=res[i-1]*nums[i];
+        vector<int>pf(n,1),sf(n,1);
+        for(int i=0;i<n;i++){
+            if(i) pf[i]*=pf[i-1];
+            pf[i]*=nums[i];
         }
-        int ans=1;
-        for(int i=n-1;i>0;i--){
-            res[i]=res[i-1]*ans;
-            ans*=nums[i];
+        for(int i= n-1;i>=0;i--){
+            if(i!=n-1) sf[i]*=sf[i+1];
+            sf[i]*=nums[i];
         }
-        res[0]=ans;
-        return res;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            if(i>0 && i<n-1){
+                ans[i]=pf[i-1]*sf[i+1];
+            }else if(i==0){
+                ans[i]=sf[i+1];
+            }else{
+                ans[i]=pf[i-1];
+            }
+        }
+        return ans;
     }
 };
