202341e3c8f7cc07e397059b88486199ce1d77c1
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2026-01-14 15:50:37 +0530

    Time: 0 ms (100%), Space: 8.9 MB (46.07%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -1,11 +1,38 @@
 class Solution {
 public:
+    bool sub(string s, string t) {
+        int l = 0, r = 0;
+        unordered_map<char, int> cnt;
+        unordered_map<char, int> running;
+        for (auto it : t)
+            cnt[it]++;
+
+        while (r < s.length()) {
+            running[s[r]]++;
+            if (r - l + 1 > t.length()) {
+                running[s[l]]--;
+                if (running[s[l]] == 0) {
+                    running.erase(s[l]);
+                }
+                l++;
+            }
+            if (r - l + 1 == t.length() && running == cnt &&
+                s.substr(l, r - l + 1) == t)
+                return true;
+
+            r++;
+        }
+        return false;
+    }
     bool queryString(string s, int n) {
-        string temp="";
-        for(int i=1;i<=n;i++){
-            string t=bitset<32>(i).to_string();  //no longer recursion function for dec to bit
-            temp=t.substr(t.find('1'));
-           if(s.find(temp) == std::string::npos) return false;
+        string temp = "";
+        for (int i = 1; i <= n; i++) {
+            string t =
+                bitset<32>(i)
+                    .to_string(); // no longer recursion function for dec to bit
+            temp = t.substr(t.find('1'));
+            if (s.find(temp) == std::string::npos)
+                return false;
         }
         return true;
     }
