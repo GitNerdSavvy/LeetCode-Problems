29c27ebb610d20d9f74e50c0a9af1bbe04772499
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2026-01-14 18:05:57 +0530

    Time: 1 ms (79.8%), Space: 102.4 MB (43.8%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
+    // int minOperations(vector<int>& nums, int x) {
+    //     int n = nums.size();
+    //     if (accumulate(nums.begin(), nums.end(), 0) < x)
+    //         return -1;
+    //     int t = 0;
+    //     int ans = 1e9;
+    //     for (int i = 0; i < n; i++) {
+    //         t += nums[i];
+    //         if (t == x) {
+    //             ans = i + 1;
+    //             break;
+    //         }
+    //     }
+    //     t = 0;
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         t += nums[i];
+    //         if (t == x) {
+    //             ans = min(ans, n - i);
+    //             break;
+    //         }
+    //     }
+    //     t = 0;
+    //     map<int, int> ump;
+    //     ump[0] = 0;
+    //     for (int i = 0; i < n - 1; i++) {
+    //         t += nums[i];
+    //         ump[t] = i + 1;
+    //     }
+    //     t = 0;
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         t += nums[i];
+    //         if (ump.find(x - t) != ump.end()) {
+    //             ans = min(ans, ump[x - t] + n - i);
+    //         }
+    //     }
+    //     return ans == 1e9 ? -1 : ans;
+    // }  greedy
+
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        if(accumulate(nums.begin(),nums.end(),0)<x) return -1;
-        int t = 0;
-        int ans=1e9;
-        for (int i = 0; i < n; i++) {
-            t += nums[i];
-            if (t == x){
-                ans=i+1;
-                break;
-            }
+        int t = accumulate(nums.begin(), nums.end(), 0) - x;
+        if(t==0) return n;
+        int l = 0, r = 0, mx = 0, csum = 0;
+        while (r < n) {
+            csum += nums[r];
+            while (l <= r && csum > t)
+                csum -= nums[l++];
+
+            if (csum == t)
+                mx = max(mx, r - l + 1);
+            r++;
         }
-        t = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            t += nums[i];
-            if (t == x){
-                ans=min(ans,n-i);
-                break;
-            }       
-        }
-        t = 0;
-        map<int, int> ump;
-        ump[0] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            t += nums[i];
-            ump[t] = i + 1;
-        }
-        t = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            t += nums[i];
-            if (ump.find(x - t) != ump.end()) {
-              ans=min(ans,ump[x - t] + n - i);
-            }
-        }
-        return ans==1e9?-1:ans;
+        return mx ? n - mx : -1;
     }
 };
